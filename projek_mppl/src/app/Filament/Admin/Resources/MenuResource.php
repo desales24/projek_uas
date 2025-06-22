ee264b0a08035b1031363315d81d5a9d12439e12
desales24 <fransiskusdes24@gmail.com>
2025-06-23 01:39:46 +0700

    first commit

--- projek_mppl/src/app/Filament/Admin/Resources/MenuResource.php
+++ projek_mppl/src/app/Filament/Admin/Resources/MenuResource.php
@@ -15,6 +15,7 @@ class MenuResource extends Resource
     protected static ?string $model = Menu::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Master Data';
 
     public static function form(Form $form): Form
     {
